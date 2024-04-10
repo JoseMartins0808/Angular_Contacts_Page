403c8bbd62b32897a821d058a5457bdb38b3da02
JoseMartins0808 <ottenazuose@hotmail.com>
2024-04-09 23:54:00 -0300

    feat: form control name

--- src/app/components/forms/contact-form/contact-form.component.ts
+++ src/app/components/forms/contact-form/contact-form.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { tick } from '@angular/core/testing';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -10,22 +9,35 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
-  name = new FormControl("");
-  email = new FormControl("");
-  message = new FormControl("");
 
-  formSubmit(event: Event) {
-    event.preventDefault();
+  contactForm = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl("")
+  })
 
-    console.log({
-      name: this.name.value,
-      email: this.email.value,
-      message: this.message.value
-    });
-
-    this.name.setValue("");
-    this.email.setValue("");
-    this.message.setValue("");
+  onSubmit() {
+    console.log(this.contactForm.value);
+    this.contactForm.reset();
+    // this.contactForm.controls.email.value;     PARA ACESSAR CADA PROPRIEDADE
   }
-
 }
+// export class ContactFormComponent {
+//   name = new FormControl("");
+//   email = new FormControl("");
+//   message = new FormControl("");
+
+//   formSubmit(event: Event) {
+//     event.preventDefault();
+
+//     console.log({
+//       name: this.name.value,
+//       email: this.email.value,
+//       message: this.message.value
+//     });
+
+//     this.name.setValue("");
+//     this.email.setValue("");
+//     this.message.setValue("");
+//   }
+// }
