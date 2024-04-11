32805c2d642562fb2afb2633296ba1440827f0e4
JoseMartins0808 <ottenazuose@hotmail.com>
2024-04-10 22:06:10 -0300

    feat: select form

--- src/app/components/forms/contact-form/contact-form.component.ts
+++ src/app/components/forms/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,14 +11,33 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class ContactFormComponent {
 
   contactForm = new FormGroup({
-    name: new FormControl(""),
-    email: new FormControl(""),
-    password: new FormControl("")
+    name: new FormControl(null, [Validators.required, Validators.min(3)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.min(8)]),
+    stack: new FormControl(""),
+    agree: new FormControl(false)
   })
 
   onSubmit() {
-    console.log(this.contactForm.value);
-    this.contactForm.reset();
+    console.log({
+      name: this.contactForm.controls.name.status,
+      email: this.contactForm.controls.email.status,
+      password: this.contactForm.controls.password.status,
+      stack: this.contactForm.controls.stack.status,
+      agree: this.contactForm.controls.agree.status
+    });
+
+    if (this.contactForm.status === "VALID") {
+      if (this.contactForm.controls.agree.value === false) {
+        alert("Você deve concordar com os termos de uso!");
+        return;
+      }
+      console.log(this.contactForm.value);
+      this.contactForm.reset({ stack: "" });
+    } else {
+      alert("Preencha todos os campos!");
+    }
+
     // this.contactForm.controls.email.value;     PARA ACESSAR CADA PROPRIEDADE
   }
 }
