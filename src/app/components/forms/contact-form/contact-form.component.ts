08080979a33ee42770f355c8d7976f4cde552d35
JoseMartins0808 <ottenazuose@hotmail.com>
2024-04-10 22:48:09 -0300

    feat: handle form errors

--- src/app/components/forms/contact-form/contact-form.component.ts
+++ src/app/components/forms/contact-form/contact-form.component.ts
@@ -1,42 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
   contactForm = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.min(3)]),
+    name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.min(8)]),
-    stack: new FormControl(""),
-    agree: new FormControl(false)
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    stack: new FormControl("", [Validators.minLength(3)]),
+    agree: new FormControl(null)
   })
 
   onSubmit() {
     console.log({
-      name: this.contactForm.controls.name.status,
-      email: this.contactForm.controls.email.status,
-      password: this.contactForm.controls.password.status,
-      stack: this.contactForm.controls.stack.status,
-      agree: this.contactForm.controls.agree.status
+      name: this.contactForm.controls.name.value,
+      email: this.contactForm.get("email")?.errors,
+      password: this.contactForm.controls.password.value,
+      stack: this.contactForm.controls.stack.value,
+      agree: this.contactForm.controls.agree.value
     });
 
-    if (this.contactForm.status === "VALID") {
-      if (this.contactForm.controls.agree.value === false) {
-        alert("Você deve concordar com os termos de uso!");
-        return;
-      }
-      console.log(this.contactForm.value);
-      this.contactForm.reset({ stack: "" });
-    } else {
-      alert("Preencha todos os campos!");
-    }
+    // if (this.contactForm.status === "VALID") {
+    //   if (this.contactForm.controls.agree.value === false) {
+    //     alert("Você deve concordar com os termos de uso!");
+    //     return;
+    //   }
+    //   console.log(this.contactForm.value);
+    //   this.contactForm.reset({ stack: "" });
+    // } else {
+    //   alert("Preencha todos os campos!");
+    // }
 
     // this.contactForm.controls.email.value;     PARA ACESSAR CADA PROPRIEDADE
   }
