701f18ef6b2b0ffac235c7f1e43238b64b01aeff
JoseMartins0808 <ottenazuose@hotmail.com>
2024-04-12 11:35:21 -0300

    feat: handle contact-form errors

--- src/app/components/forms/contact-form/contact-form.component.ts
+++ src/app/components/forms/contact-form/contact-form.component.ts
@@ -1,24 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
   contactForm = new FormGroup({
-    name: new FormControl(null),
-    email: new FormControl(null),
-    phone: new FormControl(null),
-    area: new FormControl(""),
-    message: new FormControl(null)
-  })
+    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [Validators.required]),
+    area: new FormControl("", [Validators.required]),
+    message: new FormControl(null, [Validators.required, Validators.minLength(10)])
+  });
+
+  get errors() {
+    return {
+      name: this.contactForm.get("name")?.errors,
+      email: this.contactForm.get("email")?.errors,
+      phone: this.contactForm.get("phone")?.errors,
+      area: this.contactForm.get("area")?.errors,
+      message: this.contactForm.get("message")?.errors
+    }
+  }
 
   onSubmit() {
-    console.log(this.contactForm.value);
+    if (this.contactForm.status === "VALID") {
+      console.log(this.contactForm.value);
+      this.contactForm.reset({ area: "" });
+    } else {
+      alert("Preencha corretamente");
+    }
   }
 }
