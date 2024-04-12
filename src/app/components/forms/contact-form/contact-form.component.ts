515411874ba7bf416484a30bc72fc3c24d6176a2
JoseMartins0808 <ottenazuose@hotmail.com>
2024-04-12 11:13:30 -0300

    feat: handle contact form

--- src/app/components/forms/contact-form/contact-form.component.ts
+++ src/app/components/forms/contact-form/contact-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
+  contactForm = new FormGroup({
+    name: new FormControl(null),
+    email: new FormControl(null),
+    phone: new FormControl(null),
+    area: new FormControl(""),
+    message: new FormControl(null)
+  })
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
